ceab46c9cbd895bfbf6ab0eaace2cf74e6b8abaa
Elizaveta139 <elizavetarevina139@gmail.com>
2023-11-16 15:48:10 +0200

    3 завд

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time'; //ключ
+
+player.on('timeupdate', throttle(onPlaybackTime, 1000));
+
+//відтоворення часу + локальне сховище
+function onPlaybackTime(evt) {
+  localStorage.setItem(STORAGE_KEY, evt.seconds);
+}
+
+const currentTime = localStorage.getItem(STORAGE_KEY);
+
+//відновлення відтворення зі збереженої позиції
+player.setCurrentTime(currentTime || 0);
